d3fc033b2fbb3511ad5115a07bf8e7ce682b5dff
Shivanya1912 <88928036+Shivanya1912@users.noreply.github.com>
2022-05-05 16:07:00 +0530

    Update index.js
    changed name and email

--- portfolio-template/index.js
+++ portfolio-template/index.js
@@ -1,6 +1,6 @@
-const name = 'Your Name'
-const title = 'Your Title'
-const mail = 'Your Email'
+const name = 'shivani kesharwani'
+const title = 'azure workshop'
+const mail = 'shivanikesharwani1912@gmail.com'
 
 document.getElementById('title').innerText = name
 document.getElementById('name').innerText = name
